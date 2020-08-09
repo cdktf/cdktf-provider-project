ac61615775f5efe129ec33ee6740a8777d0aa601
Sebastian Korfmann <sebastian@korfmann.net>
2020-08-09 23:49:32 +0200

    Bump version

--- src/cdktf-config.ts
+++ src/cdktf-config.ts
@@ -10,7 +10,7 @@ export class CdktfConfig {
   constructor(project: JsiiProject, options: CdktfConfigOptions) {
     const { terraformProvider } = options;
 
-    project.addPeerDependencies({cdktf: Semver.caret('0.0.13')})
+    project.addPeerDependencies({cdktf: Semver.caret('0.0.14')})
     project.addPeerDependencies({constructs: Semver.caret('3.0.4')})
     project.addScript('fetch', 'rm -rf ./src/* && cdktf get && cp -R .gen/providers/aws/* ./src/')
     project.addScript('commit', 'git add -A && git commit -am "Update provider" || echo "No changes to commit"')
@@ -19,8 +19,8 @@ export class CdktfConfig {
     project.addScript('test', 'jest --passWithNoTests')
     project.addFields({publishConfig: {access: 'public'}})
 
-    project.addDevDependencies({cdktf: Semver.caret('0.0.13')})
-    project.addDevDependencies({'cdktf-cli': Semver.caret('0.0.13')})
+    project.addDevDependencies({cdktf: Semver.caret('0.0.14')})
+    project.addDevDependencies({'cdktf-cli': Semver.caret('0.0.14')})
 
     project.npmignore.comment('cdktf config');
     project.npmignore.exclude(CDKTF_JSON_FILE);
