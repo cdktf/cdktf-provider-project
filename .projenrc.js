3aa98563b4d3368ec91f47190da40bb53fcf697e
Sebastian Korfmann <sebastian@korfmann.net>
2021-03-10 20:38:34 +0100

    Upgrade to projen 0.17

--- .projenrc.js
+++ .projenrc.js
@@ -14,6 +14,7 @@ const project = new TypeScriptProject({
     'change-case',
   ],
   license: 'MPL-2.0',
+  defaultReleaseBranch: 'master',
   releaseToNpm: true,
   minNodeVersion: '10.17.0',
   compileBeforeTest: true,
