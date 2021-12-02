0ec3daa3fb51015da0253967e19af0a00edf8466
Ansgar Mertens <ansgar@hashicorp.com>
2021-12-02 11:23:47 +0100

    chore: fix typo

--- src/readme.ts
+++ src/readme.ts
@@ -62,7 +62,7 @@ Find auto-generated docs for this provider here: [./API.md](./API.md)
 
 ## Versioning
 
-This project is explicitly not tracking the Terraform ${providerName} Provider version 1:1. In fact, it always tracks \`latest\` of \`${providerVersion}\` with every release. If there scenarios where you explicitly have to pin your provider version, you can do so by generating the [provider constructs manually](https://cdk.tf/imports).
+This project is explicitly not tracking the Terraform ${providerName} Provider version 1:1. In fact, it always tracks \`latest\` of \`${providerVersion}\` with every release. If there are scenarios where you explicitly have to pin your provider version, you can do so by generating the [provider constructs manually](https://cdk.tf/imports).
 
 These are the upstream dependencies:
 
