45b830b631c6df140a408a306763356e070c3999
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-07-28 13:09:07 +0200

    WIP

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,3 +1,6 @@
+import { execSync } from "child_process";
+import * as fs from "fs-extra";
+import path from "path";
 import { CdktfProviderProject, CdktfProviderProjectOptions } from "../src";
 import { synthSnapshot } from "./utils";
 
@@ -36,4 +39,56 @@ test("build runs without crash reporting", () => {
   const snapshot = synthSnapshot(getProject());
 
   expect(snapshot["cdktf.json"]).toHaveProperty("sendCrashReports", false);
+});
+
+describe("go builds of repositories with dash succeed", () => {
+  let outdir: string;
+  beforeAll(() => {
+    outdir = fs.mkdtempSync("google-beta-provider-test");
+  });
+
+  test("test", () => {
+    const project = getProject({
+      terraformProvider: "google-beta@~> 4.17",
+      outdir,
+    });
+    project.addDevDeps("@cdktf/provider-project@file:../");
+    project.synth();
+
+    // Create a .projenrc.js file in the outdir
+    fs.writeFileSync(
+      `${outdir}/.projenrc.js`,
+      `
+const { CdktfProviderProject } = require("@cdktf/provider-project");
+const project = new CdktfProviderProject({
+  terraformProvider: "google-beta@~> 4.17",
+  cdktfVersion: "^0.12.0",
+  constructsVersion: "^10.0.0",
+  minNodeVersion: "14.17.0",
+  jsiiVersion: "^1.53.0",
+});
+project.addDevDeps("@cdktf/provider-project@file:${path.resolve(
+        __dirname,
+        ".."
+      )}");
+project.synth();
+    `,
+      "utf8"
+    );
+
+    // fetch the provider
+    execSync(`yarn && yarn fetch`, { cwd: outdir, stdio: "inherit" });
+
+    // Run go build
+    execSync(`yarn build && yarn package:go`, {
+      cwd: outdir,
+      stdio: "inherit",
+    });
+
+    // It should have compiled successfully
+  });
+
+  afterAll(() => {
+    fs.removeSync(outdir);
+  });
 });
