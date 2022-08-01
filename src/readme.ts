1fe100ca312509570125755caf0db1ff7d6f0e95
Ansgar Mertens <ansgar@hashicorp.com>
2022-08-01 16:34:58 +0200

    feat: link to constructs.dev in readme

--- src/readme.ts
+++ src/readme.ts
@@ -24,7 +24,7 @@ export class ReadmeFile extends FileBase {
     return `
 # Terraform CDK ${providerName} Provider ${providerVersion}
 
-This repo builds and publishes the Terraform ${providerName} Provider bindings for [cdktf](https://cdk.tf).
+This repo builds and publishes the Terraform ${providerName} Provider bindings for [CDK for Terraform](https://cdk.tf).
 
 ## Available Packages
 
@@ -68,6 +68,7 @@ The go package is generated into the [\`github.com/hashicorp/cdktf-provider-${pr
 ## Docs
 
 Find auto-generated docs for this provider here: [./API.md](./API.md)
+You can also visit a hosted version of the documentation on [constructs.dev](https://constructs.dev/packages/@cdktf/provider-${providerName}).
 
 ## Versioning
 
