dd1200d8553e467ec648e5ad4eda12ecf2e5e3ea
Ansgar Mertens <ansgar@hashicorp.com>
2022-08-01 16:22:40 +0200

    feat: configure Github issues with link to terraform-cdk repo

---
+++ src/github-issues.ts
@@ -0,0 +1,44 @@
+import { cdk, FileBase, FileBaseOptions, IResolver, Project } from "projen";
+
+interface GithubIssuesConfig {
+  providerName: string;
+}
+
+/**
+ * creates files in .github/ISSUE_TEMPLATE to configure user guidance to create issues
+ */
+export class GithubIssues {
+  constructor(project: cdk.JsiiProject, config: GithubIssuesConfig) {
+    new IssueConfigFile(project, ".github/ISSUE_TEMPLATE/config.yml", config);
+  }
+}
+
+interface IssueConfigFileOptions extends FileBaseOptions {
+  providerName: string;
+}
+
+class IssueConfigFile extends FileBase {
+  protected readonly options: IssueConfigFileOptions;
+
+  constructor(
+    project: Project,
+    filePath: string,
+    options: IssueConfigFileOptions
+  ) {
+    super(project, filePath, options);
+    this.options = options;
+  }
+
+  protected synthesizeContent(resolver: IResolver) {
+    const { providerName } = resolver.resolve(
+      this.options
+    ) as IssueConfigFileOptions;
+
+    return `blank_issues_enabled: false
+contact_links:
+  - name: File an issue
+    url: "https://github.com/hashicorp/terraform-cdk/issues/new?labels=bug%2C+new%2C+pre-built+providers&template=bug-report-prebuilt-providers.md&title=\`${providerName}\`+provider:+"
+    about: Please file issues with pre-built providers in our main repository.
+`;
+  }
+}
