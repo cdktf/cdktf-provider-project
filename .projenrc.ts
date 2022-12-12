18f3513ad2d15a8a16a4497615e545ac6636473e
Nara Kasbergen <nara.kasbergen@gmail.com>
2022-12-12 13:21:13 +0100

    fix: have lock issues workflow be generated by Projen
    This required refactoring the project setup a bit to change it to
    TypeScript and allow it to import the already-existing workflow
    being used by the individual prebuilt provider repos. There might
    possibly be some issues since Projen does not make it easy to
    transition from JS to TS once the project already exists.

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,10 +1,11 @@
-const { cdk } = require("projen");
+import { cdk } from "projen";
+import { LockIssues } from "./src/lock-issues";
 
 const project = new cdk.JsiiProject({
   name: "@cdktf/provider-project",
-  authorName: "HashiCorp",
-  authorUrl: "https://hashicorp.com",
-  repository: "https://github.com/hashicorp/cdktf-provider-project.git",
+  author: "HashiCorp",
+  authorAddress: "https://hashicorp.com",
+  repositoryUrl: "https://github.com/hashicorp/cdktf-provider-project.git",
   authorOrganization: true,
   peerDeps: ["projen@^0.64.1"],
   deps: ["change-case", "fs-extra"],
@@ -14,7 +15,6 @@ const project = new cdk.JsiiProject({
   defaultReleaseBranch: "main",
   releaseToNpm: true,
   minNodeVersion: "14.17.0",
-  compileBeforeTest: true,
   mergify: false,
   scripts: {
     "eslint:fix": "eslint . --ext .ts --fix",
@@ -25,7 +25,11 @@ const project = new cdk.JsiiProject({
       labels: ["dependencies"],
     },
   },
+  projenrcTs: true,
 });
 
 project.addFields({ publishConfig: { access: "public" } });
+
+new LockIssues(project);
+
 project.synth();
