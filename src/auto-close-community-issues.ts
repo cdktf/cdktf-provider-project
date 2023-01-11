5b314da8448a81886945ade492242bbd4dcc3da2
Ansgar Mertens <ansgar@hashicorp.com>
2023-01-11 14:37:03 +0100

    feat: allow MEMBER PRs

--- src/auto-close-community-issues.ts
+++ src/auto-close-community-issues.ts
@@ -32,7 +32,7 @@ export class AutoCloseCommunityIssues {
         pullRequests: JobPermission.WRITE,
         issues: JobPermission.WRITE,
       },
-      if: "github.event.issue.author_association != 'OWNER' && github.event.issue.author_association != 'COLLABORATOR'",
+      if: "github.event.issue.author_association != 'OWNER' && github.event.issue.author_association != 'MEMBER' && github.event.issue.author_association != 'COLLABORATOR'",
       steps: [
         {
           name: "Auto-close issues by non-collaborators",
