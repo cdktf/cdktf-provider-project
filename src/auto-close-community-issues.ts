e6fe5e647c51427bb8a433051cd82226ecbce4eb
Nara Kasbergen <nara.kasbergen@gmail.com>
2023-05-31 12:19:21 +0200

    chore: autoclose community PRs
    Taking a second pass at this automation as prompted by
    cdktf/cdktf-provider-aws#1051 which suggests this is worth
    having. Also removes the peter-evans/close-issue action since
    the author himself indicates there is no need for this as it
    can be done using the gh CLI instead.

--- src/auto-close-community-issues.ts
+++ src/auto-close-community-issues.ts
@@ -10,7 +10,7 @@ interface AutoCloseCommunityIssuesOptions {
   providerName: string;
 }
 /**
- * Automatically closes issues reported by non-collaborators since this isn't the right place for them.
+ * Automatically closes issues and PRs by non-collaborators since this isn't the right place for them.
  */
 export class AutoCloseCommunityIssues {
   constructor(
@@ -18,42 +18,86 @@ export class AutoCloseCommunityIssues {
     options: AutoCloseCommunityIssuesOptions
   ) {
     const { providerName } = options;
-    const workflow = project.github?.addWorkflow("auto-close-community-issues");
+    const issueWorkflow = project.github?.addWorkflow(
+      "auto-close-community-issues"
+    );
+    const prWorkflow = project.github?.addWorkflow("auto-close-community-prs");
 
-    if (!workflow) throw new Error("no workflow defined");
+    if (!issueWorkflow) throw new Error("no workflow defined");
+    if (!prWorkflow) throw new Error("no workflow defined");
 
-    workflow.on({
+    issueWorkflow.on({
       issues: {
         types: ["opened"],
       },
     });
+    prWorkflow.on({
+      pullRequest: {
+        types: ["opened"],
+      },
+    });
 
-    workflow.addJob("autoclose", {
+    const comment =
+      `Hi there! 👋 We appreciate your interest, but this is probably not the right place. 
+      All the code in this repository is auto-generated using 
+      [cdktf-provider-project](https://github.com/cdktf/cdktf-provider-project) and 
+      [cdktf-repository-manager](https://github.com/cdktf/cdktf-repository-manager) from the source 
+      [Terraform provider](https://github.com/terraform-providers/terraform-provider-${providerName}). 
+      If there are problems, they should be addressed in one of those 3 repositories, not here, 
+      as any changes here will just get overwritten the next time there is an update upstream. 
+      Please open a new issue or PR in one of those repos. In the meantime, I'll auto-close this. 
+      Thanks!`.replace(/\s+/g, " ");
+
+    issueWorkflow.addJob("autoclose", {
       runsOn: ["ubuntu-latest"],
       permissions: {
-        pullRequests: JobPermission.WRITE,
         issues: JobPermission.WRITE,
       },
-      if: "github.event.issue.author_association != 'OWNER' && github.event.issue.author_association != 'MEMBER' && github.event.issue.author_association != 'COLLABORATOR'",
+      if: "github.event.issue.author_association != 'OWNER' && github.event.issue.author_association != 'COLLABORATOR'",
       steps: [
         {
           name: "Auto-close issues by non-collaborators",
-          uses: "peter-evans/close-issue@v2",
-          with: {
-            "issue-number": "${{ github.event.issue.number }}",
-            comment:
-              `Hi there! 👋 We appreciate your interest, but this is probably not the right place. 
-              All the code in this repository is auto-generated using 
-              [cdktf-provider-project](https://github.com/cdktf/cdktf-provider-project) and 
-              [cdktf-repository-manager](https://github.com/cdktf/cdktf-repository-manager) from the source 
-              [Terraform provider](https://github.com/terraform-providers/terraform-provider-${providerName}). 
-              If there are problems, they should be addressed in one of those 3 repositories, not here, 
-              as any changes here will just get overwritten the next time there is an update upstream. 
-              Please open a new issue or PR in one of those repos. In the meantime, I'll auto-close this. 
-              Thanks!`.replace(/\s+/g, " "),
-            "close-reason": "not_planned",
-            labels: "invalid,wontfix",
+          env: {
+            GH_TOKEN: "${{ secrets.GITHUB_TOKEN }}",
           },
+          run:
+            'gh issue close ${{ github.event.issue.number }} --reason "not planned" --comment "' +
+            comment +
+            '"',
+        },
+        {
+          name: "Add labels",
+          env: {
+            GH_TOKEN: "${{ secrets.GITHUB_TOKEN }}",
+          },
+          run: 'gh issue edit ${{ github.event.issue.number }} --add-label "invalid,wontfix"',
+        },
+      ],
+    });
+
+    prWorkflow.addJob("autoclose", {
+      runsOn: ["ubuntu-latest"],
+      permissions: {
+        pullRequests: JobPermission.WRITE,
+      },
+      if: "github.event.pull_request.author_association != 'OWNER' && github.event.pull_request.author_association != 'COLLABORATOR'",
+      steps: [
+        {
+          name: "Auto-close PRs by non-collaborators",
+          env: {
+            GH_TOKEN: "${{ secrets.GITHUB_TOKEN }}",
+          },
+          run:
+            'gh pr close ${{ github.event.pull_request.number }} --delete-branch --comment "' +
+            comment +
+            '"',
+        },
+        {
+          name: "Add labels",
+          env: {
+            GH_TOKEN: "${{ secrets.GITHUB_TOKEN }}",
+          },
+          run: 'gh pr edit ${{ github.event.pull_request.number }} --add-label "invalid,wontfix"',
         },
       ],
     });
