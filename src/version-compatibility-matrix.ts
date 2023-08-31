273d2d69a62d48efaaf8881d643e070d7c8c4cfc
Mark DeCrane <mark.decrane@hashicorp.com>
2023-08-31 15:48:03 -0400

    feat: added script for generating and updating version compatibility file

---
+++ src/version-compatibility-matrix.ts
@@ -0,0 +1,31 @@
+import { cdk } from "projen";
+import { UpdateVersionFile } from "./scripts/update-version-file";
+
+type VersionCompatibilityMatrixOptions = {
+    underlyingTerraformProviderVersion: string;
+    cdktfVersion: string;
+};
+
+export class VersionCompatibilityMatrix {
+    constructor(
+        project: cdk.JsiiProject,
+        config: VersionCompatibilityMatrixOptions
+    ) {
+        const versionFileUpdateScript = new UpdateVersionFile(project, {
+            underlyingTerraformProviderVersion:
+                config.underlyingTerraformProviderVersion,
+            cdktfVersion: config.cdktfVersion,
+        });
+        project.addTask("update-version-file", {
+            exec: `node ./${versionFileUpdateScript.path}`,
+        });
+        project.buildWorkflow?.addPostBuildSteps({
+            name: "changelog file",
+            run: "cat dist/changelog.md",
+        });
+        project.buildWorkflow?.addPostBuildSteps({
+            name: "Update Version Compatiblity File",
+            run: "yarn update-version-file",
+        });
+    }
+}
