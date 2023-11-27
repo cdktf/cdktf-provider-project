c7875967848f39bab63df93ca459d08b97737e4f
Nara Kasbergen Kwon <855115+xiehan@users.noreply.github.com>
2023-11-27 13:12:40 +0100

    chore(deps): upgrade actions/github-script & remove node-fetch (#357)

--- .projenrc.ts
+++ .projenrc.ts
@@ -14,12 +14,12 @@ import { LockIssues } from "./src/lock-issues";
 const githubActionPinnedVersions = {
   "actions/checkout": "b4ffde65f46336ab88eb53be808477a3936bae11", // v4.1.1
   "actions/download-artifact": "9bc31d5ccc31df68ecc42ccf4149144866c47d8a", // v3.0.2
-  "actions/github-script": "d7906e4ad0b1822421a7e6a35d5ca353c962f410", // v6.4.1
+  "actions/github-script": "60a0d83039c74a4aee543508d2ffcb1c3799cdea", // v7.0.1
   "actions/setup-node": "8f152de45cc393bb48ce5d89d36b731f54556e65", // v4.0.0
   "actions/upload-artifact": "a8a3f3ad30e3422c9c7b888a15615d19a852ae32", // v3.1.3
   "amannn/action-semantic-pull-request":
-    "47b15d52c5c30e94a17ec87eb8dd51ff5221fed9", // v5.3.0
-  "dessant/lock-threads": "be8aa5be94131386884a6da4189effda9b14aa21", // v4.0.1
+    "e9fabac35e210fea40ca5b14c0da95a099eff26f", // v5.4.0
+  "dessant/lock-threads": "d42e5f49803f3c4e14ffee0378e31481265dda22", // v5.0.0
   "peter-evans/create-pull-request": "153407881ec5c347639a548ade7d8ad1d6740e38", // v5.0.2
 };
 
@@ -34,11 +34,7 @@ const project = new cdk.JsiiProject({
   jsiiVersion: "^5.0.1",
   peerDeps: ["projen@^0.75.0"],
   deps: ["change-case", "fs-extra"],
-  devDeps: [
-    "@types/fs-extra",
-    "glob",
-    "node-fetch@~2", // @TODO this can be removed once we upgrade to Node 18 and use native fetch
-  ],
+  devDeps: ["@types/fs-extra", "glob"],
   bundledDeps: ["change-case", "fs-extra"],
   defaultReleaseBranch: "main",
   releaseToNpm: true,
--- src/index.ts
+++ src/index.ts
@@ -62,7 +62,7 @@ const getMavenName = (providerName: string): string => {
 const githubActionPinnedVersions = {
   "actions/checkout": "b4ffde65f46336ab88eb53be808477a3936bae11", // v4.1.1
   "actions/download-artifact": "9bc31d5ccc31df68ecc42ccf4149144866c47d8a", // v3.0.2
-  "actions/github-script": "d7906e4ad0b1822421a7e6a35d5ca353c962f410", // v6.4.1
+  "actions/github-script": "60a0d83039c74a4aee543508d2ffcb1c3799cdea", // v7.0.1
   "actions/setup-dotnet": "3447fd6a9f9e57506b15f895c5b76d3b197dc7c2", // v3.2.0
   "actions/setup-go": "93397bea11091df50f3d7e59dc26a7711a8bcfbe", // v4.1.0
   "actions/setup-java": "0ab4596768b603586c0de567f2430c30f5b0d2b0", // v3.13.0
@@ -71,8 +71,8 @@ const githubActionPinnedVersions = {
   "actions/stale": "1160a2240286f5da8ec72b1c0816ce2481aabf84", // v8.0.0
   "actions/upload-artifact": "a8a3f3ad30e3422c9c7b888a15615d19a852ae32", // v3.1.3
   "amannn/action-semantic-pull-request":
-    "47b15d52c5c30e94a17ec87eb8dd51ff5221fed9", // v5.3.0
-  "dessant/lock-threads": "be8aa5be94131386884a6da4189effda9b14aa21", // v4.0.1
+    "e9fabac35e210fea40ca5b14c0da95a099eff26f", // v5.4.0
+  "dessant/lock-threads": "d42e5f49803f3c4e14ffee0378e31481265dda22", // v5.0.0
   "hashicorp/setup-copywrite": "867a1a2a064a0626db322392806428f7dc59cb3e", // v1.1.2
   "imjohnbo/issue-bot": "6924a99d928dc228f407d34eb3d0149eda73f2a7", // v3.4.3
   "peter-evans/create-pull-request": "153407881ec5c347639a548ade7d8ad1d6740e38", // v5.0.2
