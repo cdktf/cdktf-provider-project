241ccbcebc8dcdb2e806a5c79697f333e588b2ca
Nara Kasbergen Kwon <855115+xiehan@users.noreply.github.com>
2024-07-08 14:27:38 +0200

    chore(deps): update pinned versions of GitHub Actions (#428)

--- .projenrc.ts
+++ .projenrc.ts
@@ -16,16 +16,16 @@ import { generateRandomCron, Schedule } from "./src/util/random-cron";
 // If you want to update actions versions for the individual prebuilt providers,
 // you will need to update the map in src/index.ts
 const githubActionPinnedVersions = {
-  "actions/checkout": "0ad4b8fadaa221de15dcec353f45205ec38ea70b", // v4.1.4
+  "actions/checkout": "692973e3d937129bcbf40652eb9f2f61becf3332", // v4.1.7
   "actions/download-artifact": "65a9edc5881444af0b9093a5e628f2fe47ea3b2e", // v4.1.7
   "actions/github-script": "60a0d83039c74a4aee543508d2ffcb1c3799cdea", // v7.0.1
   "actions/setup-node": "60edb5dd545a775178f52524783378180af0d1f8", // v4.0.2
   "actions/stale": "28ca1036281a5e5922ead5184a1bbf96e5fc984e", // v9.0.0
   "actions/upload-artifact": "65462800fd760344b1a7b4382951275a0abb4808", // v4.3.3
   "amannn/action-semantic-pull-request":
-    "cfb60706e18bc85e8aec535e3c577abe8f70378e", // v5.5.2
+    "0723387faaf9b38adef4775cd42cfd5155ed6017", // v5.5.3
   "dessant/lock-threads": "1bf7ec25051fe7c00bdd17e6a7cf3d7bfb7dc771", // v5.0.1
-  "peter-evans/create-pull-request": "6d6857d36972b65feb161a90e484f2984215f83e", // v6.0.5
+  "peter-evans/create-pull-request": "c5a7806660adbe173f04e3e038b0ccdcd758773c", // v6.1.0
 };
 
 const project = new cdk.JsiiProject({
@@ -147,7 +147,7 @@ project.addFields({
 project.buildWorkflow?.addPostBuildSteps(
   {
     name: "Setup Copywrite tool",
-    uses: "hashicorp/setup-copywrite@867a1a2a064a0626db322392806428f7dc59cb3e", // v1.1.2
+    uses: "hashicorp/setup-copywrite@32638da2d4e81d56a0764aa1547882fc4d209636", // v1.1.3
   },
   { name: "Add headers using Copywrite tool", run: "copywrite headers" }
 );
